51a755de9e0f8e19ade69cfdce5bf22f71253edf
Tobias Liljeblad <tobias@issuefab.com>
2022-11-07 11:24:36 +0100

    remove breaklines from text

--- Canvas/Text.ts
+++ Canvas/Text.ts
@@ -5,7 +5,7 @@ import { Options } from "./Options"
 
 export class Text extends Inline {
 	private readonly font: pdf.PDFFont
-	private readonly maxWidth: number = 400
+
 	constructor(readonly value: string, readonly options: Readonly<Options>, context: Context) {
 		const font = context.fonts[options.font.name] // TODO: add font weight and varian to name
 		super(context, {
@@ -14,13 +14,7 @@ export class Text extends Inline {
 		})
 		this.font = font
 	}
-	breakIntoLines(): Text[] {
-		return pdf
-			.breakTextIntoLines(this.value, [" ", "-"], this.maxWidth, t =>
-				this.font.widthOfTextAtSize(t, this.options.font.size)
-			)
-			.map(value => new Text(value, this.options, this.context))
-	}
+
 	render(): void {
 		this.context.page.drawText(this.value, {
 			size: this.options.font.size,
