f07ee63504b1b803dd31d3aa6e8382bfd0f3103f
Tobias Liljeblad <tobias@issuefab.com>
2022-11-08 14:05:17 +0100

    add definition model

--- DefinitionList.ts
+++ DefinitionList.ts
@@ -1,7 +1,12 @@
 import { Block } from "./Block"
 import { Canvas } from "./Canvas"
+import { Definition } from "./models/Definition"
 
 export class DefinitionList extends Block {
+	constructor(private content: Definition) {
+		super()
+		
+	}
 	getOperations(canvas: Canvas, options: Canvas.Options): Canvas.Line[] {
 		throw new Error("Method not implemented.")
 	}
