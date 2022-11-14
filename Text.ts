53cf5897e9874def299963fcd967e457d8bd41c1
Tobias Liljeblad <tobias@issuefab.com>
2022-11-14 14:45:13 +0100

    clean text.

--- Text.ts
+++ Text.ts
@@ -1,13 +1,12 @@
 import * as pdf from "pdf-lib"
-import { Operation } from "./Canvas"
-import { Options } from "./Canvas/Options"
+import { Context } from "./Canvas/Context"
 import { Inline } from "./Inline"
 
 export class Text extends Inline {
+	getOperations(context: Context): void {
+		throw new Error("Method not implemented.")
+	}
 	constructor(readonly value: string, readonly font: pdf.PDFFont) {
 		super()
-	}
-	getOperations(canvas: Operation, options: Options): Operation.Inline[] {
-		return canvas.breakIntoLines(this.value, options)
 	}
 }
