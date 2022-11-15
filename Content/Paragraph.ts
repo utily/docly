d4c5a5bb7e6960a23ba19b063e22e9f2ca559ffb
toli2003 <toli2003@student.miun.se>
2022-11-15 10:10:33 +0100

    implemented Paragraph

--- Content/Paragraph.ts
+++ Content/Paragraph.ts
@@ -1,26 +1,29 @@
 import { Bounds } from "../Bounds"
-import { Canvas } from "../Canvas/Canvas"
+import { Context } from "../Canvas/Context"
 import { Line } from "../Canvas/Line"
 import { Block } from "./Block"
 
 export class Paragraph extends Block {
 	/*
-	Need to take a text, style it with new context, return it to canvas? 
+	Need to take a text, style it with new context, return it to canvas?
 	Bounds?! Does it need to be on every
-	
-	What does a paragraph need? 
+
+	What does a paragraph need?
 	* Bounds (Internal)
 	* Style
 	*	Content
 	* Block elements, has margins around and takes full width (Bound width.)
 	*/
 
-	bounds: Bounds
-	constructor(readonly content: string) {
+	constructor(readonly content: string, readonly bounds: Bounds) {
 		super()
 	}
-	getOperations(Canvas: Canvas): Line[] {
-		throw new Error("No implementation")
+	getOperations(context: Context): Line[] {
+		const ads = context.breakIntoLines(this.content, this.bounds).map(line => context.create("line", [line]))
+		ads.unshift(context.create("line", [context.create("text", " ")]))
+		ads.push(context.create("line", [context.create("text", " ")]))
+
+		return ads
 	}
 	createBounds(): Bounds {
 		throw new Error("Method not implemented.")
